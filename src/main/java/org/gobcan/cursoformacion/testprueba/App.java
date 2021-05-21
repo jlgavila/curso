61fe50878fa550d49ddab60e79eb596d437a51eb
jlgavila <84497327+jlgavila@users.noreply.github.com>
2021-05-21 07:56:37 +0100

    La primera subida de código al repositorio GIT propio.

---
+++ src/main/java/org/gobcan/cursoformacion/testprueba/App.java
@@ -0,0 +1,19 @@
+package org.gobcan.cursoformacion.testprueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public static int suma(int a, int b) {
+		return a+b;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "La suma del 10 con el 35 es " + suma(10,35) );
+    }
+}
